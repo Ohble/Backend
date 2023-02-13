0df7d10932d992a2022c93e248f5321a1dcd1d76
K-Diger <kitten.diger@gmail.com>
2023-02-13 19:17:54 +0900

    refactor : delete Survey Domain

--- src/main/java/com/ohble/domain/survey/repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ohble.domain.survey.repository;
-
-import com.ohble.domain.survey.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
-}
