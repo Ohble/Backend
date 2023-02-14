57436ead699fa5078c08da9b98be6ead9f58f6cb
K-Diger <kitten.diger@gmail.com>
2023-02-14 15:58:32 +0900

    refactor : swagger 항목 추가

--- src/main/java/com/ohble/domain/participant/controller/ParticipantController.java
+++ src/main/java/com/ohble/domain/participant/controller/ParticipantController.java
@@ -1,6 +1,7 @@
 package com.ohble.domain.participant.controller;
 
 import com.ohble.domain.participant.service.ParticipantService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ public class ParticipantController {
 
     private final ParticipantService participantService;
 
+    @ApiOperation(value = "설문조사를 참여하는 사용자의 인덱스를 발급 받는다.")
     @PostMapping
     public Map<String, Long> generateParticipant() {
         return participantService.generateParticipant();
--- src/main/java/com/ohble/domain/surveyresult/controller/SurveyResultController.java
+++ src/main/java/com/ohble/domain/surveyresult/controller/SurveyResultController.java
@@ -3,6 +3,7 @@ package com.ohble.domain.surveyresult.controller;
 import com.ohble.domain.surveyresult.SurveyResult;
 import com.ohble.domain.surveyresult.controller.dto.SurveyResultRequestDto.SubmitSurveyRequestForm;
 import com.ohble.domain.surveyresult.service.SurveyResultService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +19,7 @@ public class SurveyResultController {
 
     private final SurveyResultService surveyResultService;
 
+    @ApiOperation(value = "설문 조사 결과를 제출한다.", notes = "질문과 그에대한 응답을 알맞게 보내주어 등록한다.")
     @ResponseStatus(OK)
     @PostMapping
     public Map<String, Boolean> submitSurveyResult(@RequestBody SubmitSurveyRequestForm submitSurveyRequestForm) {
@@ -26,6 +28,7 @@ public class SurveyResultController {
                 submitSurveyRequestForm.questionToList(), submitSurveyRequestForm.answerToList());
     }
 
+    @ApiOperation(value = "지금까지 제출된 설문조사 내역을 보여준다.", notes = "설문 조사를 제출할 때와 폼은 같다.")
     @ResponseStatus(OK)
     @GetMapping
     public List<SurveyResult> loadAllSurveyResult() {
