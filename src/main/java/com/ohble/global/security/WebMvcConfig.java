3b26444bafc9d945124f9b0e9e7d6fe2772fc197
K-Diger <kitten.diger@gmail.com>
2023-03-08 14:17:07 +0900

    fix : cors 전체 적용

--- src/main/java/com/ohble/global/security/WebMvcConfig.java
+++ src/main/java/com/ohble/global/security/WebMvcConfig.java
@@ -15,6 +15,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                    .allowedOrigins("*")
                     .allowedOrigins("http://localhost:3000")
                     .allowedOrigins("https://api.ohble.net");
             }
